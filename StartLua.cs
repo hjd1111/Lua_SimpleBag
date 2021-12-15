9d59c4d47e69f92647b787b824a693205247275d
hjd1111 <59413068+hjd1111@users.noreply.github.com>
2021-12-15 15:35:13 +0800

    Add files via upload

---
+++ StartLua.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SLua;
+
+public class StartLua : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		LuaSvr svr = new LuaSvr();
+		svr.init(null, () =>
+		{
+			svr.start("Lua_Bag/LuaScripts/StartLua");
+		});
+	}
+	
+}
